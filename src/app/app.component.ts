569efcf3f0101b7c58677dbf369425e37692a729
Youssef BENMIRA <youssef.benmira@etu.uca.fr>
2020-12-13 15:11:57 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormProperty, PropertyGroup } from 'ngx-schema-form';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testing-app';
+  
+  mySchema = 
+  {
+               "type": "object",
+               "title": "Example of a form",
+               "description": "Fill out the names of a family members.",
+               "properties": {
+                 "name": {
+                   "type": "string",
+                   "title": "Surname"
+                 },
+                 "forename": {
+                   "type": "string",
+                   "title": "Forename"
+                 },
+                 "children": {
+                   "type": "array",
+                   "title": "Family",
+                   "items": {
+                     "type": "object",
+                     "title": "Children",
+                     "properties": {
+                       "name": {
+                         "type": "string",
+                         "title": "Surname"
+                       },
+                       "forename": {
+                         "type": "string",
+                         "title": "forename"
+                       },
+                       "age": {
+                         "type": "number",
+                         "title": "age"
+                       }
+                     }
+                   }
+                 }
+               }
+             }
+
+  // Declare a mapping between field and event-id
+  myFieldBindings = {
+      '/name': [
+        {
+          'input': (event, formProperty: FormProperty) => {
+            const parent: PropertyGroup = formProperty.findRoot();
+
+            /**
+             * Set the input value for the children
+             */
+            const child1: FormProperty = parent.getProperty('children/0/name');
+
+            child1.setValue(formProperty.value, false);
+
+            const child2: FormProperty = parent.getProperty('children/1/name');
+            child2.setValue(event.target.value, false);
+
+            /**
+             * Get the input value for all the children
+             */
+            for (const objectProperty of parent.getProperty('children').properties) {
+              console.log('Value for child ', objectProperty, objectProperty.properties['name'].value);
+            }
+          }
+        }
+      ]
+    };
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SchemaFormModule, WidgetRegistry, DefaultWidgetRegistry } from "ngx-schema-form";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    SchemaFormModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [{provide: WidgetRegistry, useClass: DefaultWidgetRegistry}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
